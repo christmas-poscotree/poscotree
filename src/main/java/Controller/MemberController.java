3f878e52b83b2c86d94a4397abf00e2de68cdd49
coalong <minay0904@gmail.com>
2022-12-08 21:30:51 +0900

    [feat] 비밀번호 암호화

--- src/main/java/Controller/MemberController.java
+++ src/main/java/Controller/MemberController.java
@@ -1,16 +1,17 @@
 package Controller;
 
 import domain.Member;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import repository.MemberRepository;
 import repository.MessageRepository;
 
@@ -36,16 +37,15 @@ public class MemberController {
     }
 
     @PostMapping(value = "/login")
-    public String doLogin(@ModelAttribute Member member, HttpServletRequest request) throws SQLException {
+    public String doLogin(@ModelAttribute Member member, HttpServletRequest request) throws SQLException, NoSuchAlgorithmException {
         HttpSession session = request.getSession();
         Member loginMember = memberRepository.selectUser(member.getEmail())
                                              .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 회원 정보입니다."));
-        if (!Objects.equals(member.getPassword(), loginMember.getPassword())) {
+        if (!Objects.equals(MemberController.entyptPassword(member.getEmail(), member.getPassword()), loginMember.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 유효하지 않습니다.");
         }
 
         session.setAttribute("memberNo", loginMember.getMemberNo());
-        System.out.println("memberNumber : " + loginMember.getMemberNo());
 
         Integer userTreeNo = treeRepository.findUserTree(loginMember.getMemberNo());
         if (Objects.isNull(userTreeNo)) {
@@ -68,13 +68,21 @@ public class MemberController {
     }
 
     @PostMapping(value = "/signup")
-    public String doSignup(@ModelAttribute Member member) throws SQLException {
+    public String doSignup(@ModelAttribute Member member) throws SQLException, NoSuchAlgorithmException {
         if (memberRepository.checkedEmail(member.getEmail())) {
+            member.setPassword(MemberController.entyptPassword(member.getEmail(), member.getPassword()));
             memberRepository.insert(member);
         } else {
             throw new IllegalArgumentException("이미 가입된 이메일입니다.");
         }
         return "redirect:/";
+    }
+
+    static String entyptPassword(String id,String pw) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        // 평문+id 암호화
+        md.update((id+pw).getBytes());
+        return String.format("%064x", new BigInteger(1, md.digest()));
     }
 
 }
